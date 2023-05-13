f8ff7e747371d0c579f5e154d4616afcf157f845
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-05-13 15:47:25 +0200

    hw10

--- src/script/fetchCountries.js
+++ src/script/fetchCountries.js
@@ -1,10 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
-const searchParams = new URLSearchParams({
-    fields: 'name,capital,population,flags,languages,',
-});
 
  function fetchCountries (name){
-    const url = `${BASE_URL}${name}?${searchParams}`;
+    const url = `${BASE_URL}${name}?fields=name,capital,population,flags,languages`;
     return fetch(url).then(response => {
         if (response.status === 404) {
             throw new Error(response.status);
